14f09fae0add2d29d9634489ce845275eb0a20de
Alex7D3 <ao11@my.yorku.ca>
2025-09-21 23:50:54 -0400

    Update command register script

--- bot/src/utils/register-commands.ts
+++ bot/src/utils/register-commands.ts
@@ -1,10 +1,14 @@
 import { loadCommands } from './get-commands';
+import path from 'node:path';
+import url from 'node:url';
 import { REST, Routes, RESTPutAPIApplicationCommandsJSONBody } from 'discord.js';
 const { discord_token, discord_client_id, guild_id } = process.env;
 
 const rest = new REST({ version: '10' }).setToken(discord_token);
 
-const commandList = await loadCommands('../commands')
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const commandsPath = path.join(__dirname, '../commands');
+const commandList = await loadCommands(commandsPath)
   .then(commandMap => [...commandMap.values()].map(command => command.data));
 
 try {
